591ba9151ed57d4b3b92a7990769721360fd714b
daydayupys <3698010150@qq.com>
2026-06-07 16:14:22 +0800

    第一个版本，实现内存模型及“load_const”

---
+++ PBInterpreter.py
@@ -0,0 +1,28 @@
+from fault import LoadFault
+
+class PBInterpreter:
+    
+    def __init__(self, const: list[tuple[str, int]] = [], instruction: list = []):
+        self.stack = []
+        self.global_var = {}
+        self.const = const
+        self.instruction = instruction
+
+    def run(self):
+        ins_list = {"load_const": self._load_const}
+        pc = 0
+        pc_max = len(self.instruction)
+        while pc < pc_max:
+            opname, arg = self.instruction[pc]
+            result = ins_list[opname](arg)
+            if result == 0:
+                pc += 1
+                
+    def _load_const(self, arg):
+        try:
+            self.stack.append(self.const[arg])
+        except Exception:
+            raise LoadFault("所加载的字面量不存在") from None
+        return 0
+               
+        
---
+++ example.py
@@ -0,0 +1,9 @@
+from PBInterpreter import PBInterpreter
+
+Interpreter = PBInterpreter(["hello", "world"], 
+
+[("load_const", 2)] #故意越界
+
+)
+
+Interpreter.run()
